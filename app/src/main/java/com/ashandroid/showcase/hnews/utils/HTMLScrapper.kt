8d7b96d4b1ab8268bcd15fa98d07871ca1c624ba
praburaam-jm <pjayaraman@onewillow.com>
2022-03-18 20:45:31 +0530

    Implementing simple html scrapper that reads first 100 words of a webpage

---
+++ app/src/main/java/com/ashandroid/showcase/hnews/utils/HTMLScrapper.kt
@@ -0,0 +1,47 @@
+package com.ashandroid.showcase.hnews.utils
+
+import org.jsoup.Jsoup
+
+class HTMLScrapper {
+
+    fun parseContent(url: String, callback: (String?) -> Unit, totalWords: Int = 100) {
+        val doc = Jsoup.connect(url).get()
+        val body = doc.body()
+        var wcount = 0
+        var lineNo = 0
+        var content = ""
+
+        val strBuilder = StringBuilder()
+        body.allElements.forEach {
+            val paragraph = it.getElementsByTag("p").text()
+            //skip the first paragraph
+            if (lineNo > 0) {
+                strBuilder.appendLine(paragraph)
+                wcount += countWords(paragraph)
+            } else {
+                val firstCount = countWords(paragraph)
+                if (firstCount > 20) {
+                    strBuilder.appendLine(paragraph)
+                    wcount += countWords(paragraph)
+                }
+            }
+
+            if (wcount >= totalWords) {
+                return@forEach
+            }
+            lineNo++
+        }
+        callback(firstNWords(strBuilder.toString(), totalWords))
+    }
+
+    private fun firstNWords(content: String, totalWords: Int): String? {
+        val wordsList = content.split(" ")
+        val possibleWords = if (wordsList.size < totalWords) wordsList.size else totalWords
+        val neededWordsList = wordsList.subList(0, possibleWords)
+        return neededWordsList.joinToString(" ").capitalize()
+    }
+
+    fun countWords(paragraph: String): Int {
+        return paragraph.split(" ").size
+    }
+}
