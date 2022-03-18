159fa9c5601cce01c87d6c80e5e4a69c542f2c0e
praburaam-jm <pjayaraman@onewillow.com>
2022-03-18 23:52:26 +0530

    Listening to viewmodel variables and showing progress bar

--- app/src/main/java/com/ashandroid/showcase/hnews/ui/fragment/AskStoriesFragment.kt
+++ app/src/main/java/com/ashandroid/showcase/hnews/ui/fragment/AskStoriesFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ashandroid.showcase.hnews.StoriesViewModel
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.ashandroid.showcase.hnews.adapter.StoriesAdapter
 import com.ashandroid.showcase.hnews.adapter.StoriesListener
 import com.ashandroid.showcase.hnews.databinding.FragmentAskStoriesBinding
+import kotlinx.android.synthetic.main.fragment_ask_stories.*
+import kotlinx.android.synthetic.main.fragment_top_stories.*
+import kotlinx.android.synthetic.main.fragment_top_stories.recycler_view
 
 class AskStoriesFragment : Fragment() {
 
     private val viewModel: StoriesViewModel by activityViewModels()
+    var n = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +27,7 @@ class AskStoriesFragment : Fragment() {
 
         val binding = FragmentAskStoriesBinding.inflate(inflater)
         // Get getAskStoriesList() from ViewModel for list of items
-        viewModel.getAskStories()
+        viewModel.getAskStories(n)
         // lifecycleOwner – The LifecycleOwner that should be used for observing changes of LiveData in this binding.
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -34,6 +39,24 @@ class AskStoriesFragment : Fragment() {
 
         return binding.root
 
+    }
+
+    private fun scrollListener() {
+        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    n++
+                    viewModel.getTopStories(n)
+                }
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scrollListener()
     }
 
 }
