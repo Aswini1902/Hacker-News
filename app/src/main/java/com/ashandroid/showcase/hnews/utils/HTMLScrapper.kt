0bb5c77df76a88106d16dadae21a2f5efeb6f482
Aswini <aswini.nivisha2021@gmail.com>
2022-03-19 13:28:18 +0530

    Adding exception handling

--- app/src/main/java/com/ashandroid/showcase/hnews/utils/HTMLScrapper.kt
+++ app/src/main/java/com/ashandroid/showcase/hnews/utils/HTMLScrapper.kt
@@ -1,37 +1,47 @@
 package com.ashandroid.showcase.hnews.utils
 
+import org.jsoup.HttpStatusException
 import org.jsoup.Jsoup
+import java.lang.Exception
 
 class HTMLScrapper {
 
     fun parseContent(url: String, callback: (String?) -> Unit, totalWords: Int = 100) {
-        val doc = Jsoup.connect(url).get()
-        val body = doc.body()
-        var wcount = 0
-        var lineNo = 0
-        var content = ""
 
-        val strBuilder = StringBuilder()
-        body.allElements.forEach {
-            val paragraph = it.getElementsByTag("p").text()
-            //skip the first paragraph
-            if (lineNo > 0) {
-                strBuilder.appendLine(paragraph)
-                wcount += countWords(paragraph)
-            } else {
-                val firstCount = countWords(paragraph)
-                if (firstCount > 20) {
+        try {
+            val doc = Jsoup.connect(url).get()
+            val body = doc.body()
+            var wcount = 0
+            var lineNo = 0
+            var content = ""
+
+            val strBuilder = StringBuilder()
+            body.allElements.forEach {
+                val paragraph = it.getElementsByTag("p").text()
+                //skip the first paragraph
+                if (lineNo > 0) {
                     strBuilder.appendLine(paragraph)
                     wcount += countWords(paragraph)
+                } else {
+                    val firstCount = countWords(paragraph)
+                    if (firstCount > 20) {
+                        strBuilder.appendLine(paragraph)
+                        wcount += countWords(paragraph)
+                    }
                 }
-            }
 
-            if (wcount >= totalWords) {
-                return@forEach
+                if (wcount >= totalWords) {
+                    return@forEach
+                }
+                lineNo++
             }
-            lineNo++
+            callback(firstNWords(strBuilder.toString(), totalWords))
+        }catch (e: HttpStatusException){
+            callback("")
+        }catch (e: Exception){
+            callback("")
         }
-        callback(firstNWords(strBuilder.toString(), totalWords))
+
     }
 
     private fun firstNWords(content: String, totalWords: Int): String? {
