f484f67e07631aa091b07231cfa20c654cc257c1
Aswini <aswini.nivisha2021@gmail.com>
2022-03-19 13:29:28 +0530

    Fixing the import (topstory element was wrongly imported)

--- app/src/main/java/com/ashandroid/showcase/hnews/ui/detail/JobStoryDetailFragment.kt
+++ app/src/main/java/com/ashandroid/showcase/hnews/ui/detail/JobStoryDetailFragment.kt
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment
 import com.ashandroid.showcase.hnews.StoriesViewModel
 import androidx.fragment.app.activityViewModels
 import com.ashandroid.showcase.hnews.databinding.FragmentJobStoryDetailBinding
-import kotlinx.android.synthetic.main.fragment_top_story_detail.url
+import kotlinx.android.synthetic.main.fragment_job_story_detail.*
+
 
 class JobStoryDetailFragment : Fragment() {
 
